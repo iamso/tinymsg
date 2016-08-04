037ec378fc97badbc08783a266b15c53009e55ea
iamso <so@dev.so>
2016-08-04 14:48:58 +0200

    add client as git submodule

--- index.js
+++ index.js
@@ -8,21 +8,25 @@ var port = process.env.PORT || 7777;
 
 var ejs = require('ejs');
 var fs = require('fs');
-var client = fs.readFileSync(__dirname + '/client/build/client.js','utf8');
-var clientMin = fs.readFileSync(__dirname + '/client/build/client.min.js','utf8');
+var clients = {
+  'client.js': fs.readFileSync(__dirname + '/client/build/embed/client.js','utf8'),
+  'client.min.js': fs.readFileSync(__dirname + '/client/build/embed/client.min.js','utf8'),
+  'client.next.js': fs.readFileSync(__dirname + '/client/build/embed/client.next.js','utf8'),
+  // 'client.next.min.js': fs.readFileSync(__dirname + '/client/build/embed/client.next.min.js','utf8'),
+}
 
 var channels = {};
 
-app.all(/^\/client\.min(\.js)?/i, function(req, res) {
-  res.header('Cache-Control', 'public, max-age=2592000');
-  res.contentType('application/javascript');
-  console.log('min');
-  res.send(ejs.render(clientMin, {url: (req.headers['x-forwarded-proto'] === 'https' ? 'wss' : 'ws') + '://' + req.get('host')}));
-});
-app.all(/^\/client(\.js)?/i, function(req, res) {
-  res.header('Cache-Control', 'public, max-age=2592000');
-  res.contentType('application/javascript');
-  res.send(ejs.render(client, {url: (req.headers['x-forwarded-proto'] === 'https' ? 'wss' : 'ws') + '://' + req.get('host')}));
+app.all('/:client', function(req, res, next) {
+  var client = clients[req.params.client];
+  if (client) {
+    res.header('Cache-Control', 'public, max-age=2592000');
+    res.contentType('application/javascript');
+    res.send(ejs.render(client, {url: (req.headers['x-forwarded-proto'] === 'https' ? 'wss' : 'ws') + '://' + req.get('host')}));
+  }
+  else {
+    next();
+  }
 });
 
 app.use(function (req, res) {
