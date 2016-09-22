105bc7497d34e64d399db30168f673d5b4db52b9
iamso <so@dev.so>
2016-09-22 23:14:13 +0200

    use es6 syntax

--- index.js
+++ index.js
@@ -1,41 +1,43 @@
-var server = require('http').createServer();
-var url = require('url');
-var WebSocketServer = require('ws').Server;
-var wss = new WebSocketServer({ server: server });
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 7777;
+const server = require('http').createServer();
+const url = require('url');
+const WebSocketServer = require('ws').Server;
+const wss = new WebSocketServer({ server: server });
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 7777;
 
-var ejs = require('ejs');
-var fs = require('fs');
-var clients = {
-  'client.js': fs.readFileSync(__dirname + '/client/build/embed/client.js','utf8'),
-  'client.min.js': fs.readFileSync(__dirname + '/client/build/embed/client.min.js','utf8'),
-  'client.next.js': fs.readFileSync(__dirname + '/client/build/embed/client.next.js','utf8'),
-  // 'client.next.min.js': fs.readFileSync(__dirname + '/client/build/embed/client.next.min.js','utf8'),
+const ejs = require('ejs');
+const fs = require('fs');
+const clients = {
+  'client.js': fs.readFileSync(`${__dirname}/client/build/embed/client.js`,'utf8'),
+  'client.min.js': fs.readFileSync(`${__dirname}/client/build/embed/client.min.js`,'utf8'),
+  'client.next.js': fs.readFileSync(`${__dirname}/client/build/embed/client.next.js`,'utf8'),
+  // 'client.next.min.js': fs.readFileSync(`${__dirname}/client/build/embed/client.next.min.js`,'utf8'),
+  // 'client.babel.js': fs.readFileSync(`${__dirname}/client/build/embed/client.babel.js`,'utf8'),
+  // 'client.babel.min.js': fs.readFileSync(`${__dirname}/client/build/embed/client.babel.min.js`,'utf8'),
 }
 
-var channels = {};
+const channels = {};
 
-app.all('/:client', function(req, res, next) {
-  var client = clients[req.params.client];
+app.all('/:client', (req, res, next) => {
+  const client = clients[req.params.client];
   if (client) {
     res.header('Cache-Control', 'public, max-age=2592000');
     res.contentType('application/javascript');
-    res.send(ejs.render(client, {url: (req.headers['x-forwarded-proto'] === 'https' ? 'wss' : 'ws') + '://' + req.get('host')}));
+    res.send(ejs.render(client, {url: `${(req.headers['x-forwarded-proto'] === 'https' ? 'wss' : 'ws')}://${req.get('host')}`}));
   }
   else {
     next();
   }
 });
 
-app.use(function (req, res) {
+app.use((req, res) => {
   res.render('index.ejs');
 });
 
-wss.on('connection', function connection(ws) {
-  var location = url.parse(ws.upgradeReq.url, true);
-  var channel = ws.protocol || location.query.channel;
+wss.on('connection', (ws) => {
+  const location = url.parse(ws.upgradeReq.url, true);
+  const channel = ws.protocol || location.query.channel;
 
   if (Array.isArray(channels[channel])) {
     channels[channel].push(ws);
@@ -44,17 +46,19 @@ wss.on('connection', function connection(ws) {
     channels[channel] = [ws];
   }
 
-  ws.on('message', function(message) {
-    channels[channel].forEach(function(w) {
+  ws.on('message', (message) => {
+    channels[channel].forEach((w) => {
       w !== ws && w.send(message);
     });
   });
 
-  ws.on('close', function(){
+  ws.on('close', () => {
     channels[channel].splice(channels[channel].indexOf(ws), 1);
   });
 
 });
 
 server.on('request', app);
-server.listen(port, function () { console.log('Listening on ' + server.address().port) });
+server.listen(port, () => {
+  console.log(`Listening on ${server.address().port}`);
+});
