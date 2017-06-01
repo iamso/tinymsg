43e26ae0a062ce97744e9f25283b9d3001bbc584
iamso <so@dev.so>
2017-06-01 17:50:03 +0200

    add http interface for sending messages to a channel

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const WebSocketServer = require('ws').Server;
 const wss = new WebSocketServer({ server: server });
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const multipart  = require('connect-multiparty');
+const compress   = require('compression');
+const cors       = require('cors');
+const helmet     = require('helmet');
 const port = process.env.PORT || 7777;
 
 const ejs = require('ejs');
@@ -17,6 +22,14 @@ fs.readdirSync(clientsBase).forEach(file => {
   clients[file] = fs.readFileSync(`${clientsBase}${file}`, 'utf-8');
 });
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.text({ type: 'text/plain' }));
+app.use(multipart());
+app.use(cors());
+app.use(helmet());
+app.use(compress());
+
 app.all('/:client', (req, res, next) => {
   const client = clients[req.params.client];
   if (client) {
@@ -29,12 +42,39 @@ app.all('/:client', (req, res, next) => {
   }
 });
 
+app.all('/send/:channel', (req, res, next) => {
+  const channel = req.params.channel;
+  let message = req.query.message || req.body.message || req.body;
+  let status;
+
+
+  if (typeof message === 'object') {
+    try {
+      message = JSON.stringify(message);
+    }
+    catch(e) {}
+  }
+
+
+  if (message && typeof message === 'string' && Array.isArray(channels[channel])) {
+    channels[channel].forEach((w) => {
+      w.send(message);
+    });
+    status = "message sent";
+  }
+  else {
+    status = "channel unavailable";
+  }
+
+  res.json({status});
+});
+
 app.use((req, res) => {
   res.render('index.ejs');
 });
 
-wss.on('connection', (ws) => {
-  const location = url.parse(ws.upgradeReq.url, true);
+wss.on('connection', (ws, req) => {
+  const location = url.parse(req.url, true);
   const channel = ws.protocol || location.query.channel;
 
   if (Array.isArray(channels[channel])) {
